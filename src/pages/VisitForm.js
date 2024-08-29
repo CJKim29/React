f9f05594aaec8800b21a93f370c07525344ed0bc
CJKim29 <kchanjong29@gmail.com>
2024-08-29 13:31:14 +0900

    240829

--- src/pages/VisitForm.js
+++ src/pages/VisitForm.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { useMediaQuery } from 'react-responsive';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function VisitForm() {
 
@@ -15,8 +18,6 @@ function VisitForm() {
         query: "(max-width:767px)"
     });
 
-
-
     let screen_style = { margin: 'auto', minWidth: '600px', width: '600px' };
     if (isPc) {
         screen_style = { margin: 'auto', minWidth: '600px', width: '600px' };
@@ -25,19 +26,70 @@ function VisitForm() {
     } else if (isMobile) {
         screen_style = { margin: 'auto', minWidth: '400px', width: '90%' };
     }
+
+    const [visit, setVisit] = useState({ name: "", content: "", pwd: "" });
+
+    //화면전환객체
+    const navigator = useNavigate();
+
+    function updateVisit(e) {
+        let id = e.target.id;
+        let value = e.target.value;
+        setVisit({ ...visit, [id]: value });
+    }
+    function addVisit() {
+
+        //입력값 체크
+        if (visit.name === '') {
+            alert("이름을 입력하세요");
+            return;
+        }
+
+        if (visit.content === '') {
+            alert("내용을 입력하세요");
+            return;
+        }
+
+        if (visit.pwd === '') {
+            alert("비밀번호를 입력하세요");
+            return;
+        }
+
+        // POST 요청(insert) : parameter는 JSON형식
+        const url = "http://localhost:8080/rest/visit";
+        axios.post(url, visit)
+            .then(function (response) {
+                console.log(response.data);
+                if (response.data.result === false) {
+                    alert("등록 실패");
+                    return;
+                }
+
+                //화면초기화(필요는 없다)
+                //setVisit({ name: "", content: "", pwd: "" })
+
+                //목록보기 화면으로 이동
+                navigator("/");
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     return (
         <div>
             <div style={screen_style}>
                 <form className="form-inline">
                     <h2 style={{ color: '#999', fontSize: '20px' }}>♣ 방명록 작성하기 ♣</h2>
                     <br />
-                    <input className="form-control" id="name" placeholder="작성자명을 입력하세요" />
+                    <input className="form-control" id="name" value={visit.name} placeholder="작성자명을 입력하세요" onChange={updateVisit} />
                     <br />
-                    <textarea className="form-control" style={{ width: '100%', resize: 'none' }} id="content" rows={5} cols={50} placeholder="내용을 입력하세요"></textarea>
+                    <textarea className="form-control" style={{ width: '100%', resize: 'none' }} id="content" value={visit.content}
+                        rows={5} cols={50} placeholder="내용을 입력하세요" onChange={updateVisit}></textarea>
                     <br />
-                    <input className="form-control" type="password" id="pwd" placeholder="비밀번호 입력하세요" />
+                    <input className="form-control" type="password" id="pwd" value={visit.pwd} placeholder="비밀번호 입력하세요" onChange={updateVisit} />
                     <br />
-                    <input className="btn btn-success mybtn" type="button" value="글올리기" />
+                    <input className="btn btn-success mybtn" type="button" value="글올리기" onClick={addVisit} />
                 </form>
             </div>
 
--- src/pages/VisitList.js
+++ src/pages/VisitList.js
@@ -1,15 +1,141 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useMediaQuery } from "react-responsive";
+import { IconButton } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function VisitList() {
 
+    const isPc = useMediaQuery({
+        query: "(min-width:1024px)"
+    });
+
+    const isTablet = useMediaQuery({
+        query: "(min-width:768px) and (max-width:1023px)"
+    });
+
+    const isMobile = useMediaQuery({
+        query: "(max-width:767px)"
+    });
+
+
+
+    let screen_style = { margin: 'auto', minWidth: '600px', width: '600px' };
+    if (isPc) {
+        screen_style = { margin: 'auto', minWidth: '600px', width: '600px' };
+    } else if (isTablet) {
+        screen_style = { margin: 'auto', minWidth: '500px', width: '90%' };
+    } else if (isMobile) {
+        screen_style = { margin: 'auto', minWidth: '400px', width: '90%' };
+    }
+
+    const [list, setList] = useState([]); // [] <- 초기값 빈 배열
+
     useEffect(() => {
         //Data 읽어오는 코드
+        //console.log("--useEffect()--");
+        getData();
     }, []);
+
+    function getData() {
+        const url = "http://localhost:8080/rest/visits";
+        // GET 요청(select)
+        axios.get(url)
+            .then(function (response) {
+                console.log(response.data);
+
+                //useState()정보가 갱신 -> Re-Rendering
+                setList(response.data.list);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    function deleteVisit(idx) {
+
+        const c_pwd = prompt("삭제 비밀번호:");
+
+        //취소
+        if (c_pwd == null) return;
+
+        //입력되지 않은 경우
+        if (c_pwd === "") {
+            alert("삭제할 비밀번호를 입력하세요");
+            return;
+        }
+
+        // GET 요청(select) : 비밀번호 확인요청
+        const url = `http://localhost:8080/rest/check-pwd/${idx}/${c_pwd}`;
+        axios.get(url)
+            .then(function (response) {
+                console.log(response);
+                if (response.data.result === false) {
+                    alert("비밀번호가 틀렸습니다");
+                    return;
+                }
+
+                //삭제처리
+                deleteVisitData(idx);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }//end:deleteVisit
+
+    function deleteVisitData(idx) {
+
+        // DELETE 요청(delete)
+        const url = "http://localhost:8080/rest/visit/" + idx;
+        axios.delete(url)
+            .then(function (response) {
+                console.log(response);
+                if (response.data.result === false) {
+                    alert("삭제 실패");
+                    return;
+                }
+
+                //삭제확인
+                if (window.confirm("정말 삭제하시겠습니까?") === false) return;
+
+                // 새로운 데이터 요청
+                getData();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }//end:deleteVisitData
+
     return (
-        <div>
-            방명록 목록
-        </div>
+        <main>
+            {/* <div className="flex flex-col items-center justify-center h-full"> */}
+
+            <div className="flex flex-col h-full" style={screen_style}>
+                {list &&
+                    list.map((v, index) => (
+                        <div key={index} style={{ border: '1px solid black', margin: '10px', padding: '20px', boxShadow: '1px 1px 3px black' }}>
+
+                            {/* 첫번째줄 */}
+                            <div className="flex flex-row">
+                                <span style={{ display: 'inline-block', width: '80%', textAlign: 'left', color: 'black', fontWeight: 'bold' }} >
+                                    ♥ {v.name}({v.pwd})
+                                </span>
+                                <span style={{ display: 'inline-block', width: '20%', textAlign: 'right' }}>
+                                    <IconButton aria-label="delete" id={v.idx} onClick={() => deleteVisit(v.idx)}>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </span>
+                            </div>
+                            {/* 두번째줄 */}
+                            <p style={{ textAlign: 'left', color: 'gray', whiteSpace: 'pre-wrap' }}>{v.content.replaceAll('<br>', '\n')}</p>
+                            {/* 세번째줄 */}
+                            <p style={{ textAlign: 'left', color: '#ccc' }}>{v.regdate}</p>
+                        </div>
+                    ))}
+            </div>
+        </main>
     );
-}
+};
 
 export default VisitList;
